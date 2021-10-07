67804055423bc4e3f15b0a900074586022a09c45
Remish Varsani <scspl.remish@gmail.com>
2021-10-07 17:05:05 +0530

    test

--- routers/user_route.js
+++ routers/user_route.js
@@ -3,7 +3,6 @@ const authMiddleware = require('../middleware/auth_middleware');
 const userRoute = express.Router();
 const { registerUser,loginUser,verifyOTP,changePassword,forgotPassword,updateProfile,logoutUser } = require('../controllers/user_controller');
 
-
 userRoute.route('/registerUser').post(registerUser);
 userRoute.route('/loginUser').get(loginUser);
 userRoute.route('/verifyOTP').post(authMiddleware,verifyOTP);
